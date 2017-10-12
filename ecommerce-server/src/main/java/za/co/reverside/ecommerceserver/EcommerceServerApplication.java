ace1b88b796d46111cdb85663e4055fa15cce661
Soumya <SoumyaP@discovery.co.za>
2017-10-12 11:42:30 +0200

    maven added to springboot app

---
+++ ecommerce-server/src/main/java/za/co/reverside/ecommerceserver/EcommerceServerApplication.java
@@ -0,0 +1,12 @@
+package za.co.reverside.ecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServerApplication.class, args);
+	}
+}
