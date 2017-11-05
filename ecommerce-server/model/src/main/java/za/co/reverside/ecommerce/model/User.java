e47aeff603eab0b261506c107c79e342c0c8ae5e
Soumya <SoumyaP@discovery.co.za>
2017-11-05 18:24:21 +0200

    added models

---
+++ ecommerce-server/model/src/main/java/za/co/reverside/ecommerce/model/User.java
@@ -0,0 +1,60 @@
+package za.co.reverside.ecommerce.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private int phone;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
