d308b76112eaa69e0d3d7c568f503474bf211108
Soumya <SoumyaP@discovery.co.za>
2017-11-05 18:26:12 +0200

    added springboot sample for usermanagement

---
+++ ecommerce-server/usermanagement/src/main/java/za/co/reverside/ecommerce/usermanagement/UserManagementApplication.java
@@ -0,0 +1,11 @@
+package za.co.reverside.ecommerce.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
+}
